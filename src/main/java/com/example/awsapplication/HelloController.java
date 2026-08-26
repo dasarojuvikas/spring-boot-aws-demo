3b4c219f94e93b270122c651bee44591d52523f0
dasarojuvikas <vtu15504@veltech.edu.in>
2026-08-26 16:03:00 +0530

    fix

--- src/main/java/com/example/awsapplication/HelloController.java
+++ src/main/java/com/example/awsapplication/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "aws console test run phn rohin";
+        return "aws console test run phn vikas";
     }
 }
