70985ae54d6280db5c57564cc57117479266cea7
dasarojuvikas <vtu15504@veltech.edu.in>
2026-08-26 16:55:42 +0530

    updates

--- src/main/java/com/example/awsapplication/HelloController.java
+++ src/main/java/com/example/awsapplication/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "aws console test run phn vikas";
+        return "The Real Aws deployment is working!";
     }
 }
