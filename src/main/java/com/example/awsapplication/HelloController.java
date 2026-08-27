1f172975199dc260177e41d3db86c456d42895d9
dasarojuvikas <vtu15504@veltech.edu.in>
2026-08-27 22:43:40 +0530

    FIX

--- src/main/java/com/example/awsapplication/HelloController.java
+++ src/main/java/com/example/awsapplication/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "The Real Aws deployment is working! yes----no";
+        return "The Real Aws deploymnt done bNVGH";
     }
 }
